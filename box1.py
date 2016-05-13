1ea47f90372df132967c397e77b948f256e2ad25
Jack Jiang <jackywathy24@gmail.com>
2016-05-13 20:45:51 +1000

    New boxes!

--- box1.py
+++ box1.py
@@ -14,46 +14,66 @@ import stat
 # some constants
 if os.name == 'nt':
     deskTop = os.path.expanduser("~\\Desktop\\")
+    deskTop = ''
+
 else:
     deskTop = os.path.expanduser("~/Desktop/")
 
 
+laserThickness = 0.01
+
+
 class BaseShape:
-        def save_desktop(self):
-            prev_point = None
-            drawing = dxf.drawing(deskTop + 'output.dxf')
+    def save(self):
+        """A function with joins all the points in self.allpoints together"""
+        prev_point = None
+        drawing = dxf.drawing(deskTop + 'output.dxf')
 
-            for list_points in self.all_points:
+        for list_points in self.all_points:
 
-                for point in list_points:
-                    if prev_point:
-                        drawing.add(dxf.line(prev_point, point, thickness = 2, color= 1))
-                    prev_point = point
+            for point in list_points:
+                if prev_point:
+                    drawing.add(dxf.line(prev_point, point, thickness=laserThickness, color=1))
+                prev_point = point
 
-            drawing.add(dxf.line(prev_point, self.all_points[0][0], thickness=2,color=1))
+        drawing.add(dxf.line(prev_point, self.all_points[0][0], thickness=laserThickness, color=1))
 
-            #os.chmod(deskTop + 'output.dxf', stat.S_IWRITE)
+        try:
+            os.chmod(deskTop + 'output.dxf', stat.S_IWRITE)
             drawing.save()
-#            os.chmod(deskTop + 'output.dxf', stat.S_IREAD)
+            os.chmod(deskTop + 'output.dxf', stat.S_IREAD)
+        except FileNotFoundError:
+            drawing.save()
 
 
 class Base(BaseShape):
     """The Base shape"""
     def __init__(self, height_base, width_base, height_box, mat_thickness, margin_error = 2):
         #bottom left corner is 0,0
-        # create the bassseee
+        #create the bassseee temp stuff~!
         bottom_left = [(mat_thickness, mat_thickness)]
         top_left = [(mat_thickness, height_base - mat_thickness)]
         top_right = [(width_base-mat_thickness, height_base - mat_thickness)]
         bottom_right =  [(width_base - mat_thickness, mat_thickness)]
+
+        # set the self-variable
         self.mat_thickness = mat_thickness
         self.all_points = [bottom_left, bottom_right, top_right, top_left]
         self.correction = margin_error
         self.width = width_base
         self.height = height_base
         self.height_box = height_box
-        self.depression_bottom = None
-        self.depression_side = None
+
+        # non-esistant ones- storage for the depression and notch sizes
+        # [length, notches]
+        self.depression_bottom = [None,None]
+        self.depression_side = [None,None]
+        self.notch_side = [None,None]
+        self.notch_bottom = [None,None]
+
+
+
+        # linked pieces
         self.piece_bottom = None
         self.piece_side = None
 
@@ -64,62 +84,41 @@ class Base(BaseShape):
         
 
         
-    def save(self):
-        prev_point = None
-        drawing = dxf.drawing(deskTop + 'output.dxf')
 
-        for list_points in self.all_points:
 
-            for point in list_points:
-                if prev_point:
-                    drawing.add(dxf.line(prev_point, point, thickness=2, color=1))
-                prev_point = point
-
-        drawing.add(dxf.line(prev_point, self.all_points[0][0], thickness=2, color=1))
-
-        #os.chmod(deskTop + 'output.dxf', stat.S_IWRITE)
-        drawing.save()
-        #os.chmod(deskTop + 'output.dxf', stat.S_IREAD)
-
-    def add_notch(self, numberNotchBottom = None, lengthNotchBottom=None, numberNotchSide = None, lengthNotchSide=None):
+    def add_notch(self, notch_bottom_length, notch_bottom_number, notch_side_length, notch_side_number):
         """Adds all the notches, given the length/number of notches"""
 
         # calcs to find the distance:
         # if both values are given
+        #notch_bottom_length, notch_side_number
 
+        if (notch_side_number *  (notch_bottom_length + self.mat_thickness )) > self.width:
+            print("The width is too short")
+            raise BaseException
+        elif (notch_side_number * (notch_side_length + self.mat_thickness )) > self.height:
+            print("The height is too short")
+            raise BaseException
+        # measure the length of the bottom notches
+        self.depression_bottom[0] = Decimal(self.width - (notch_bottom_length * notch_bottom_number) - (2 * self.mat_thickness)) / notch_bottom_number
+        self.depression_side[0] = Decimal(self.height - (notch_side_length * notch_side_number) - (2 * self.mat_thickness)) / notch_side_number
+        self.notch_bottom[1] = notch_bottom_number
+        self.notch_side[1] = notch_side_number
+        self.depression_side[1] = notch_side_number + 1
+        self.depression_bottom[1] = notch_bottom_number + 1
+        self.notch_side[0] = notch_side_length
+        self.notch_bottom[0] = notch_bottom_length
 
-        if numberNotchBottom and numberNotchSide and numberNotchSide and lengthNotchSide:
-            if (numberNotchBottom * numberNotchSide) * 2 > self.width:
-                print("The width is too short")
-                raise BaseException
-            elif (numberNotchSide * lengthNotchSide) * 2 > self.height:
-                print("The height is too short")
-                raise BaseException
-            # measure the length of the bottom notches
-            self.depression_bottom = Decimal(self.width - (numberNotchBottom * lengthNotchBottom) - (2 * self.mat_thickness)) / numberNotchBottom
-            self.depression_side = Decimal(self.height - (numberNotchSide * lengthNotchSide) - (2 * self.mat_thickness)) / numberNotchSide
-            # the depression about the base and sides
-            
-
-        # the main list to select from
-        self.numberNotchBottom = numberNotchBottom
-        self.lengthNotchBottom = lengthNotchBottom
-        self.numberNotchSide = numberNotchSide
-        self.lengthNotchSide = lengthNotchSide
-
-
-
-
-        # main driver code: give it number_notch, len_notch and depression_side and depression_bottom
+        # the depression about the base and sides
+        # main driver code: crap loads of arguments and it will do you bidding!
         #start with bottom left corner
-        print('dpression', self.depression_bottom, self.depression_side)
 
         point_num = 0
         for side in range(4):
             point = self.all_points[point_num][0]
             point_num += 1
-            self.all_points[side] += segment_creator_base(side, self.depression_bottom, self.depression_side, numberNotchBottom, numberNotchSide,
-                                                          lengthNotchBottom, lengthNotchSide, point, self.mat_thickness)
+            self.all_points[side] += segment_creator_base(side, self.depression_bottom[0], self.depression_side[0], notch_bottom_length, notch_side_length,
+                                                          notch_bottom_number, notch_bottom_length, point, self.mat_thickness)
 
     def create_part_side(self):
         self.piece_side1 = Side_side(self)
@@ -137,23 +136,26 @@ class Side_side(BaseShape):
         self.height = baseObject.height_box
         self.baseObject = baseObject
 
-        self.bottomNotchLength = baseObject.depression_side
-        self.bottomNotchNumber = baseObject.numberNotchSide + 1
+
+        # some more values
+        self.notch_bottom = baseObject.depression_side
+        self.notch_side = [None,None]
+        self.depress_side = [None,self.notch_bottom[1] - 1]
+        self.depress_bottom = [None,None]
+
 
         self.mat_thickness = baseObject.mat_thickness
 
-
+        # the points
         self.bottomLeft = [(0, 0)]
         self.bottomRight = [(self.width - self.mat_thickness, 0)]
         self.topRight = [(self.width - self.mat_thickness, self.height)]
         self.topLeft = [(0, self.height)]
-
         self.allpoints = [self.bottomLeft, self.bottomRight, self.topRight, self.topLeft]
 
-        self.depress_bottom = Decimal(self.width - (self.bottomNotchLength * (self.bottomNotchNumber-2)) - 4 * self.mat_thickness) / (self.bottomNotchNumber - 1)
+        # fidn the size of depressions
+        self.depress_bottom[0] = Decimal(self.width - (self.notch_bottom[1] * (self.notch_bottom[0]-2)) - 4 * self.mat_thickness) / (self.depress_side[1])
 
-
-        self.add_notch()
 
     def add_notch(self):
 
@@ -163,9 +165,12 @@ class Side_side(BaseShape):
             point_num += 1
             self.allpoints[iteration] += segment_creator_side(iteration, self.depress_bottom, 0, self.bottomNotchNumber, 0, self.bottomNotchLength,0, self.allpoints[iteration][0], self.mat_thickness)
 
-    def height_notch(self, numberNotch = None, lenNotch = None):
-        if numberNotch and lenNotch:   # calculate the depression of the height notches
-            self.depress_height = Decimal(self.height - (4 * self.mat_thickness) - (numberNotch * lenNotch)) / (numberNotch - 1)
+    def height_notch(self, length_notch, number_notch):
+        """Create the depress side and notch_side variables. Include two end bits as part of number_notch"""
+        self.depress_side[0]  = Decimal(self.height - (4 * self.mat_thickness) - (length_notch* number_notch)) / (number_notch-1)
+        self.depress_side[1] = number_notch -1
+
+        self.add_notch()
 
 class Side_bottom:
     """The two sides on the bottom and top. The right bit is less big"""
@@ -300,12 +305,12 @@ def segment_creator_side(direction,depress_bottom, depress_side, num_notch_botto
 
 
 
-size_height = 400
+size_height = 100
 size_width = 200
-base = Base(size_height,size_width, 100, 5)
+base = Base(size_height,size_width, 50, 5)
 
 
-base.add_notch(4, 30, 4, 50)
+base.add_notch(10,5,7,5)
 
 base.save()
 
