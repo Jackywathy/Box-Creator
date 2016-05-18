5463fafb6032595af70468eb6841c1449cf35f96
Jack Jiang <jackywathy24@gmail.com>
2016-05-18 20:19:31 +1000

    GRAPHICS?

---
+++ graphics.py
@@ -0,0 +1,25 @@
+import box1 as Engine
+from tkinter import *
+
+class Application:
+    """My application!"""
+
+    """
+    SOME FRAMES
+    """
+
+
+
+    def __init__(self):
+        self.root = Tk()
+
+    def start_loop(self):
+        self.root.startloop()
+
+    def destroy_self(self):
+        self.root.destroy()
+
+    def user_interface(self):
+        print()
+
+
